6303aab5766f5b7d9ef26736e5e1de57ed2bf04a
Kabir S. Tamari <kabirtamari@gmail.com>
2020-03-06 14:11:07 +0545

    static enable

--- simple_im/settings.py
+++ simple_im/settings.py
@@ -121,9 +121,9 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.0/howto/static-files/
+# https://docs.djangoproject.com/en/2.2/howto/static-files/
+
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
@@ -137,6 +137,3 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 django_heroku.settings(locals())
 
 AUTH_USER_MODEL = 'user_handler.CustomUserBase'
-LOGIN_REDIRECT_URL = ''
-LOGIN_URL='/login'
-
