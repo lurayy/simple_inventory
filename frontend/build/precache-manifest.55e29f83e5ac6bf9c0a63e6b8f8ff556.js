b30c32e63d646c3133f5800be96f2a2deaafffd1
Kabir S. Tamari <kabirtamari@gmail.com>
2020-04-03 17:06:55 +0545

    new build

---
+++ frontend/build/precache-manifest.55e29f83e5ac6bf9c0a63e6b8f8ff556.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95fc69f6a197734c3e6d3ce4ef8f841c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe2c47f4609e02ca0c19",
+    "url": "/static/css/2.b90ce945.chunk.css"
+  },
+  {
+    "revision": "a7c43bea48cb7f67af01",
+    "url": "/static/css/main.f907b1f0.chunk.css"
+  },
+  {
+    "revision": "fe2c47f4609e02ca0c19",
+    "url": "/static/js/2.d8150177.chunk.js"
+  },
+  {
+    "revision": "0c886a92816a4b437bf7a8aa7d38b109",
+    "url": "/static/js/2.d8150177.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7c43bea48cb7f67af01",
+    "url": "/static/js/main.c0c4550b.chunk.js"
+  },
+  {
+    "revision": "055e94f6602794288cc3",
+    "url": "/static/js/runtime-main.1f1f19a3.js"
+  }
+]);
