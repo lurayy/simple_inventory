805762a5a5afe814cdeb229a112040f8e51eaf8f
Kabir S. Tamari <kabirtamari@gmail.com>
2020-04-07 15:16:17 +0545

    rebuild for deployment

--- frontend/build/precache-manifest.9c09d8146cc0827f8fa78b8a6446fa57.js
+++ frontend/build/precache-manifest.9c09d8146cc0827f8fa78b8a6446fa57.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "fb115ab113bc85dde9931d64e9ab8da7",
+    "revision": "cb5e5ffa9f26da40a981b4adeb26d456",
     "url": "/index.html"
   },
   {
-    "revision": "f78de1ba546ecb9c5f88",
+    "revision": "c150d1bb9852c0988fa4",
     "url": "/static/css/2.b90ce945.chunk.css"
   },
   {
-    "revision": "6894a09350c38897f7c7",
+    "revision": "65479c849afedaf418ed",
     "url": "/static/css/main.f907b1f0.chunk.css"
   },
   {
-    "revision": "f78de1ba546ecb9c5f88",
-    "url": "/static/js/2.4c7cc2ca.chunk.js"
+    "revision": "c150d1bb9852c0988fa4",
+    "url": "/static/js/2.4dba8157.chunk.js"
   },
   {
     "revision": "0c886a92816a4b437bf7a8aa7d38b109",
-    "url": "/static/js/2.4c7cc2ca.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.4dba8157.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "6894a09350c38897f7c7",
-    "url": "/static/js/main.38a5fa30.chunk.js"
+    "revision": "65479c849afedaf418ed",
+    "url": "/static/js/main.f3bd70e6.chunk.js"
   },
   {
     "revision": "055e94f6602794288cc3",
--- frontend/build/service-worker.js
+++ frontend/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.906d95081ecd52dd2571262e950c0af5.js"
+  "/precache-manifest.9c09d8146cc0827f8fa78b8a6446fa57.js"
 );
 
 self.addEventListener('message', (event) => {
