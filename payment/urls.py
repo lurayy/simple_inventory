91dcc0fe8cf9b66eac84d4a025a39bce2ed3b45d
Kabir S. Tamari <kabirtamari@gmail.com>
2020-05-15 14:54:02 +0545

    jwt implementation and custom authoriazation completed

--- payment/urls.py
+++ payment/urls.py
@@ -2,10 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('giftcards', views.gift_cards, name='handle multiple gift cards get and add'),
-    path('giftcard', views.gift_card, name='handle single gift card get and edit'),
+    path('giftcards/get', views.get_multiple_gift_cards, name='handle multiple gift cards get and add'),
+    path('giftcard/get', views.get_gift_card_details, name='handle single gift card get and edit'),
     path('giftcards/delete', views.delete_gift_cards, name='handle multiple gift cards delete'),
     path('giftcard/validate', views.validate_gift_card, name='handle multiple gift cards delete'),
-    path('methods', views.payment_methods, name='Payment methods'),    
-    path('do',views.apply_payment, name='apply payment')
+    path('giftcard/uniquecard/delete', views.delete_unique_cards, name='delete unique cards'),
+    path('giftcard/update', views.update_gift_card, name="update gift card"),
+
+    path('payment/methods/get', views.get_payment_methods, name='Payment methods'),    
+    path('payment/do',views.apply_payment, name='apply payment')
 ]
--- payment/views.py
+++ payment/views.py
@@ -1,18 +1,19 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.http import require_http_methods
-from django.contrib.auth.decorators import login_required
 import json
 from django.core.exceptions import ObjectDoesNotExist
 from django.db import IntegrityError
 from .utils import gift_card_categories_to_json, unique_cards_to_json, gift_cards_to_json, gift_card_to_json, payment_methods_to_json
 from .models import GiftCard, GiftCardCategory, UniqueCard, Payment, PaymentMethod
 from sales.models import Invoice
+from user_handler.permission_check import bind, check_permission
 
-@login_required
+
 @require_http_methods(['POST'])
-def gift_cards(request):
-    if request.method == "POST":
+@bind
+def get_multiple_gift_cards(request):
+    if check_permission(self.__name__, request.headers['Authorization'].split(' ')[1]):
         response_json = {'status':False, 'gift_cards':[]}
         try:
             json_str = request.body.decode(encoding='UTF-8')
@@ -32,16 +33,19 @@ def gift_cards(request):
                 if data_json['filter'] == 'code':
                     response_json['gift_cards'] = gift_cards_to_json(GiftCard.objects.filter(code__icontains=str(data_json['name']).lower())[start:end])
                     response_json['status'] = True
-                    return JsonResponse(response_json)
+                return JsonResponse(response_json)
         except (KeyError, json.decoder.JSONDecodeError, IntegrityError, ObjectDoesNotExist, Exception) as exp:
             return JsonResponse({'status':False,'error': f'{exp.__class__.__name__}: {exp}'})
+    else:
+        return JsonResponse({'status':False, "error":'You are not authorized.'})
 
 
-@login_required
+
 @require_http_methods(['POST'])
-def gift_card(request):
-    response_json = {}
-    if request.method == "POST":
+@bind
+def get_gift_card_details(request):
+    response_json = {'status':False}
+    if check_permission(self.__name__, request.headers['Authorization'].split(' ')[1]):
         try:
             json_str = request.body.decode(encoding='UTF-8')
             data_json = json.loads(json_str)
@@ -50,16 +54,19 @@ def gift_card(request):
                 if data_json['filter'] == "uuid":
                     response_json['status'] = True
                     response_json['gift_cards']  = gift_card_to_json(GiftCard.objects.get(uuid=data_json['uuid']))
-                    return JsonResponse(response_json)
+                return JsonResponse(response_json)
         except (KeyError, json.decoder.JSONDecodeError, IntegrityError, ObjectDoesNotExist, Exception) as exp:
             return JsonResponse({'status':False,'error': f'{exp.__class__.__name__}: {exp}'})
+    else:
+        return JsonResponse({'status':False, "error":'You are not authorized.'})
 
 
-@login_required
+
 @require_http_methods(['POST'])
+@bind
 def delete_gift_cards(request):
-    response_json = {'status':''}
-    if request.method == "POST":
+    response_json = {'status':False}
+    if check_permission(self.__name__, request.headers['Authorization'].split(' ')[1]):
         try:
             json_str = request.body.decode(encoding='UTF-8')
             data_json = json.loads(json_str)
@@ -72,14 +79,45 @@ def delete_gift_cards(request):
             return JsonResponse(response_json)
         except (KeyError, json.decoder.JSONDecodeError, IntegrityError, ObjectDoesNotExist, Exception) as exp:
             return JsonResponse({'status':False,'error': f'{exp.__class__.__name__}: {exp}'})
+    else:
+        return JsonResponse({'status':False, "error":'You are not authorized.'})
 
-
-
-@login_required
 @require_http_methods(['POST'])
+@bind
+def update_gift_card(request):
+    response_json = {'status':False}
+    if check_permission(self.__name__, request.headers['Authorization'].split(' ')[1]):
+        try:
+            json_str = request.body.decode(encoding='UTF-8')
+            data_json = json.loads(json_str)
+            uuid = data_json['gift_card_uuid']
+            gift_card = GiftCard.objects.get(uuid=data_json['uuid'])
+            if gift_card.count_used > 1:
+                raise Exception("You cannot edit gift card that are already being used.")
+            gift_card.name = data_json['name']
+            gift_card.category = GiftCardCategory.objects.get(id=data_json['category']) 
+            gift_card.discount_type = data_json['discount_type']
+            gift_card.rate = data_json['rate']
+            gift_card.count_limit = data_json['count_limit']
+            gift_card.is_limited = data_json['is_limited']
+            gift_card.has_unique_codes = data_json['has_unique_codes']
+            gift_card.is_active = data_json['is_active']
+            gift_card.save()
+            response_json['status'] = True
+            return JsonResponse(response_json)
+        except (KeyError, json.decoder.JSONDecodeError, EmptyValueException, Exception) as exp:
+            return JsonResponse({'status':False,'error': f'{exp.__class__.__name__}: {exp}'})
+    else:
+        return JsonResponse({'status':False, "error":'You are not authorized.'})
+
+
+
+
+@require_http_methods(['POST'])
+@bind
 def delete_unique_cards(request):
-    response_json = {'status':''}
-    if request.method == "POST":
+    response_json = {'status':False}
+    if check_permission(self.__name__, request.headers['Authorization'].split(' ')[1]):
         try:
             json_str = request.body.decode(encoding='UTF-8')
             data_json = json.loads(json_str)
@@ -95,33 +133,50 @@ def delete_unique_cards(request):
         except (KeyError, json.decoder.JSONDecodeError, IntegrityError, ObjectDoesNotExist, Exception) as exp:
             return JsonResponse({'status':False,'error': f'{exp.__class__.__name__}: {exp}'})
 
-@login_required
+
 @require_http_methods(['POST'])
+@bind
 def validate_gift_card(request):
-    response_json = {'status':''}
-    if request.method == "POST":
+    response_json = {'status':False}
+    if check_permission(self.__name__, request.headers['Authorization'].split(' ')[1]):
         try:
             json_str = request.body.decode(encoding='UTF-8')
             data_json = json.loads(json_str)
             code = data_json['code']
             try:
-                card = UniqueCard.objects.get(code=code)
-                if card.is_used:
-                    response_json['status'] = False
-                    response_json['msg'] = "Card is already used."
-                else:
-                    response_json['status'] = True
-                    response_json['msg'] = "Valid"
-                    response_json['card_detail'] = {
-                        'discountType':card.gift_card.discount_type,
-                        'rate':card.gift_card.rate,
-                        'name':card.gift_card.name,
-                        'category':card.gift_card.category
-                    }
+                try:
+                    card = UniqueCard.objects.get(code=code)
+                    if card.is_used:
+                        response_json['status'] = False
+                        response_json['msg'] = "Card is already used."
+                    else:
+                        response_json['status'] = True
+                        response_json['msg'] = "Valid"
+                        response_json['card_detail'] = {
+                            'discountType':card.gift_card.discount_type,
+                            'rate':card.gift_card.rate,
+                            'name':card.gift_card.name,
+                            'category':card.gift_card.category,
+                            'isUnique':card.has_unique_codes
+                        }
+                except:
+                    card = GiftCard.objects.get(code=code)
+                    if card.is_used_out():
+                        response_json['status'] = False
+                        response_json['msg'] = "Card is already used."
+                    else:
+                        response_json['status'] = True
+                        response_json['msg'] = "Valid"
+                        response_json['card_detail'] = {
+                            'discountType':card.discount_type,
+                            'rate':card.rate,
+                            'name':card.name,
+                            'category':card.category,
+                            'isUnique':card.has_unique_codes
+                        }
             except:
                 response_json['status'] = False
                 response_json['msg'] = "Card Doesnot Exsist."
-            print(response_json)
             return JsonResponse(response_json)
         except (KeyError, json.decoder.JSONDecodeError, IntegrityError, ObjectDoesNotExist, Exception) as exp:
             return JsonResponse({'status':False,'error': f'{exp.__class__.__name__}: {exp}'})
@@ -134,59 +189,79 @@ def validate_gift_card(request):
 #     transaction_id
 # }
 
-@login_required
+
 @require_http_methods(['POST'])
-def payment_methods(request):
+@bind
+def get_payment_methods(request):
     response_json = {'status':'', 'payment_methods':''}
-    try:  
-        json_str = request.body.decode(encoding='UTF-8')
-        data_json = json.loads(json_str)
-        if data_json['action'] == 'get':
-            pay_methods = PaymentMethod.objects.filter(is_active=True)
-            response_json['payment_methods'] = payment_methods_to_json(pay_methods)
-            response_json['status'] = True
-        return JsonResponse(response_json)
-    except (KeyError, json.decoder.JSONDecodeError, IntegrityError, ObjectDoesNotExist, Exception) as exp:
-        return JsonResponse({'status':False,'error': f'{exp.__class__.__name__}: {exp}'})
+    if check_permission(self.__name__, request.headers['Authorization'].split(' ')[1]):    
+        try:  
+            json_str = request.body.decode(encoding='UTF-8')
+            data_json = json.loads(json_str)
+            if data_json['action'] == 'get':
+                pay_methods = PaymentMethod.objects.filter(is_active=True)
+                response_json['payment_methods'] = payment_methods_to_json(pay_methods)
+                response_json['status'] = True
+            return JsonResponse(response_json)
+        except (KeyError, json.decoder.JSONDecodeError, IntegrityError, ObjectDoesNotExist, Exception) as exp:
+            return JsonResponse({'status':False,'error': f'{exp.__class__.__name__}: {exp}'})
+    else:
+        return JsonResponse({'status':False, "error":'You are not authorized.'})
 
 
-@login_required
+
 @require_http_methods(['POST'])
+@bind
 def apply_payment(request):
-    response_json = {'status':''}
-    try:
-        json_str = request.body.decode(encoding='UTF-8')
-        data_json = json.loads(json_str)
-        payment_method = PaymentMethod.objects.get(id=data_json['payment_method'])
-        if (payment_method.is_gift_card):
-            card = UniqueCard.objects.get(code = data_json['transaction_from'])
-            if card.is_used:
-                raise Exception('The gift card is already used.')
-            invoice = Invoice.objects.get(id=data_json['invoice_id'])
-            if card.gift_card.discount_type == "percent":
-                amount = invoice.bill_amount * card.gift_card.rate/100
+    response_json = {'status':False}
+    if check_permission(self.__name__, request.headers['Authorization'].split(' ')[1]):    
+        try:
+            json_str = request.body.decode(encoding='UTF-8')
+            data_json = json.loads(json_str)
+            payment_method = PaymentMethod.objects.get(id=data_json['payment_method'], is_active=True)
+            if (payment_method.is_gift_card):
+                if data_json['isCardUnique']:
+                    unique_card = UniqueCard.objects.get(code = data_json['transaction_from'])
+                    if unique_card.is_used:
+                        raise Exception('The gift card is already used.')
+                    card = unique_card.gift_card
+                else:
+                    card = GiftCard.objects.get(code = data_json['transaction_from'], is_active=True)
+                    if card.is_used_out():
+                        raise Exception("This gift card is used out.")
+                
+                invoice = Invoice.objects.get(id=data_json['invoice_id'])
+                if card.discount_type == "percent":
+                    amount = invoice.bill_amount * card.rate/100
+                else:
+                    amount = card.gift_card.rate
+                payment = Payment.objects.create(
+                    invoice = invoice,
+                    payment_method = PaymentMethod.objects.get(id=data_json['payment_method']),
+                    amount = amount,
+                    transaction_from = data_json['transaction_from']
+                )
+                payment.save()
+                if data_json['isCardUnique']:
+                    unique_card.is_used = True
+                    unique_card.save()
+                else:
+                    card.count_used = card.count_used + 1
+                card.save()
+                invoice.save()
             else:
-                amount = card.gift_card.rate
-            payment = Payment.objects.create(
-                invoice = invoice,
-                payment_method = PaymentMethod.objects.get(id=data_json['payment_method']),
-                amount = amount,
-                transaction_from = data_json['transaction_from']
-            )
-            payment.save()
-            card.is_used.save()
-            invoice.save()
-        else:
-            payment = Payment.objects.create(
-                invoice = Invoice.objects.get(id=data_json['invoice_id']),
-                payment_method = PaymentMethod.objects.get(id=data_json['payment_method']),
-                amount = data_json['amount'],
-                transaction_from = data_json['transaction_from'],
-                transaction_id = data_json['transaction_id'],
-                bank_name = data_json['bank_name']
-            )
-            payment.save()
-        response_json['status'] = True
-        return JsonResponse(response_json)
-    except (KeyError, json.decoder.JSONDecodeError, IntegrityError, ObjectDoesNotExist, Exception) as exp:
-        return JsonResponse({'status':False,'error': f'{exp.__class__.__name__}: {exp}'})
+                payment = Payment.objects.create(
+                    invoice = Invoice.objects.get(id=data_json['invoice_id']),
+                    payment_method = PaymentMethod.objects.get(id=data_json['payment_method']),
+                    amount = data_json['amount'],
+                    transaction_from = data_json['transaction_from'],
+                    transaction_id = data_json['transaction_id'],
+                    bank_name = data_json['bank_name']
+                )
+                payment.save()
+            response_json['status'] = True
+            return JsonResponse(response_json)
+        except (KeyError, json.decoder.JSONDecodeError, IntegrityError, ObjectDoesNotExist, Exception) as exp:
+            return JsonResponse({'status':False,'error': f'{exp.__class__.__name__}: {exp}'})
+    else:
+        return JsonResponse({'status':False, "error":'You are not authorized.'})
