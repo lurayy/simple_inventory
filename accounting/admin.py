4cb88a33c268406d99178ac47f25b34332af7cf0
Kabir S. Tamari <kabirtamari@gmail.com>
2020-05-31 10:44:16 +0545

    account type add on adminpanal

--- accounting/admin.py
+++ accounting/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
-from .models import LedgerEntry, Account, EntryType, MonthlyStats, DefaultLedgerEntry, AccountingSettings
+from .models import LedgerEntry, Account, EntryType, MonthlyStats, DefaultLedgerEntry, AccountingSettings, AccountType
 
 admin.site.register(LedgerEntry)
 admin.site.register(Account)
 admin.site.register(EntryType)
 admin.site.register(MonthlyStats)
-
+admin.site.register(AccountType)
 admin.site.register(AccountingSettings)
 admin.site.register(DefaultLedgerEntry)
