67db25d3bf5600918229df40defd0491f58ba949
Kabir S. Tamari <kabirtamari@gmail.com>
2020-06-08 11:49:55 +0545

    updated to standards

--- sales/views.py
+++ sales/views.py
@@ -446,9 +446,10 @@ def update_customer_categories(self, request):
         try: 
             json_str = request.body.decode(encoding='UTF-8')
             data_json = json.loads(json_str)
-            category = CustomerCategory.objects.get(id=data_json['cusomter_category_id'])
-            category.name = data_json['name']
-            category.is_active = data_json['is_active']
+            if data_json['action'] == "update":
+                category = CustomerCategory.objects.get(id=data_json['cusomter_category_id'])
+                category.name = data_json['name']
+                category.is_active = data_json['is_active']
             return JsonResponse(response_json)
         except (KeyError, json.decoder.JSONDecodeError, IntegrityError, ObjectDoesNotExist, Exception) as exp:
                 return JsonResponse({'status':False,'error': f'{exp.__class__.__name__}: {exp}'})
@@ -466,9 +467,10 @@ def add_customer_categories(self, request):
             response_json['status'] = True
             json_str = request.body.decode(encoding='UTF-8')
             data_json = json.loads(json_str)
-            category = CustomerCategory.objects.create(name=data_json['name'])
-            category.save()
-            response_json['category'] = categories_to_json([category])
+            if data_json['action'] == "add":
+                category = CustomerCategory.objects.create(name=data_json['name'])
+                category.save()
+                response_json['category'] = categories_to_json([category])
             return JsonResponse(response_json)
         except (KeyError, json.decoder.JSONDecodeError, IntegrityError, ObjectDoesNotExist, Exception) as exp:
                 return JsonResponse({'status':False,'error': f'{exp.__class__.__name__}: {exp}'})
@@ -484,14 +486,15 @@ def delete_customer_categories(self, request):
         try:
             json_str = request.body.decode(encoding='UTF-8')
             data_json = json.loads(json_str)
-            ids = data_json['customer_category_ids']
-            if ids is None:
-                raise Exception('Empty List')
-            for id in ids:
-                customer = CustomerCategory.objects.get(id=int(id))
-                customer.is_active = False
-                customer.save()
-            response_json['status'] = True
+            if data_json['action'] == "delete":
+                ids = data_json['customer_category_ids']
+                if ids is None:
+                    raise Exception('Empty List')
+                for id in ids:
+                    customer = CustomerCategory.objects.get(id=int(id))
+                    customer.is_active = False
+                    customer.save()
+                response_json['status'] = True
             return JsonResponse(response_json)
         except (KeyError, json.decoder.JSONDecodeError, Exception) as exp:
             return JsonResponse({'status':False,'error': f'{exp.__class__.__name__}: {exp}'})
